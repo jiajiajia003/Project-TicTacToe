fa3a1f37b23edf6ba128990816e954bb3dfcd521
jiajiajia003 <yangmaoa01@gmail.com>
2025-06-14 23:04:59 -0700

    Redo the project
    More features want to add:
    1. Improve the way to get input of player names
    2. Pop window when win/tie.

--- javascript.js
+++ javascript.js
@@ -1,199 +1,101 @@
-function Cell() {
-    let value = "";
-
-    const takeSpot = (player) => {
-        value = player;
-    }
-
-    const getValue = () => value;
-
-    return {takeSpot, getValue};
-}
-
-function Gameboard () {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    const resetBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j++) {
-                board[i].push(Cell());
-            }
-        }
-    }
-    
-    const getBoard = () => board;
-
-    const makeMove = (row, column, playerToken) => {
-        const cell = board[row][column];
-        cell.takeSpot(playerToken);
-    }
-
-    return {getBoard, makeMove, resetBoard};
-}
-
-function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
-    const board = Gameboard();
-    let gameOver ;
-    let activePlayer;
-    let lastMessage;
+const gameController = () => {
+    let board, activePlayer, gameOver;
+    const playerOneName = prompt("Player one's Name", "Player one");
+    const playerTwoName = prompt("Player two's Name", "Player two");
     const players = [
-        {name: playerOneName, token: "X"},
-        {name: playerTwoName, token: "O"}
+        {name: playerOneName, marker: "X"},
+        {name: playerTwoName, marker: "O"}
     ];
 
-    const startNewGame = () => {
-        board.resetBoard();
-        gameOver = false;
+    const resetBoard = () => {
+        board = new Array(9).fill("");
         activePlayer = players[0];
-        lastMessage = "";
+        gameOver = false;
     }
-    
-    const switchPlayerTurn = () => {
+
+    const makeMove = (index) => {
+        if (board[index] !== "" || gameOver !== false) return;
+        board[index] = activePlayer;
+        
+        isGameOver();
+        if (gameOver === true) {
+            return
+        } else if (gameOver === "tie") {
+            return
+        }
+
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
 
+    const isGameOver = () => {
+        if (
+            (board[0] !=="" && board[0] === board[1] && board[1] === board[2]) || 
+            (board[3] !=="" && board[3] === board[4] && board[4] === board[5]) || //[3,4,5]
+            (board[6] !=="" && board[6] === board[7] && board[7] === board[8]) || //[6,7,8]
+            (board[0] !=="" && board[0] === board[3] && board[3] === board[6]) || //[0,3,6]
+            (board[1] !=="" && board[1] === board[4] && board[4] === board[7]) || //[1,4,7]
+            (board[2] !=="" && board[2] === board[5] && board[5] === board[8]) || //[2,5,8]
+            (board[0] !=="" && board[0] === board[4] && board[4] === board[8]) || //[0,4,8]
+            (board[2] !=="" && board[2] === board[4] && board[4] === board[6]) //[2,4,6]
+        ) {
+            gameOver = true;
+        } else if (!board.includes("")){
+            gameOver = "tie";
+        }
+    }
+
+    const getBoard = () => board;
     const getActivePlayer = () => activePlayer;
-    const getLastMessage = () => lastMessage;
+    const getResult = () => gameOver;
 
-    const playRound = (row, column) => {
-        lastMessage = "";
-        if (gameOver === true) {
-            startNewGame();            
-        }
-        if (isMoveValid(row, column)) {
-            board.makeMove(row, column, getActivePlayer().token);
-            const curBoard = board.getBoard(); 
-            if (isWinning(curBoard, row, column)) {
-                lastMessage = `${getActivePlayer().name} wins!`;
-                gameOver = true;
-            } else if (isTie(curBoard)) {
-                lastMessage = "Game Ties"
-                gameOver = true;
-            } else {
-                switchPlayerTurn();
-            }
-        }
-    }
+    resetBoard();
 
-    const isTie = (curBoard) => {
-        for (let row of curBoard) {
-            for (let cell of row) {
-                if (cell.getValue() === "") {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    return {getBoard, makeMove, resetBoard, getActivePlayer, getResult}
+};
 
-    const isWinning = (curBoard, row, column) => {
-        const winByRow = isThree(curBoard[row][0], curBoard[row][1], curBoard[row][2]);
-        const winByColumn = isThree(curBoard[0][column], curBoard[1][column], curBoard[2][column]);
-        let winByDiagonal1 = false;
-        let winByDiagonal2 = false;
-
-        if (row === column) {
-            winByDiagonal1 = isThree(curBoard[0][0], curBoard[1][1], curBoard[2][2]);
-        }
-        if (row + column === 2) {
-            winByDiagonal2 = isThree(curBoard[0][2], curBoard[1][1], curBoard[2][0]);
-        }
-
-        return winByRow || winByColumn || winByDiagonal1 || winByDiagonal2;
-    }
-
-    const isThree = (cell1, cell2, cell3) => {
-        let result;
-        return result = (cell1.getValue() === cell2.getValue() && cell1.getValue() === cell3.getValue()) ? true : false;
-    }
-
-    const isMoveValid = (row, column) => {
-        let moveIsValid;
-        return moveIsValid = board.getBoard()[row][column].getValue() === "" ? true : false;
-    }
-
-    startNewGame();
-
-    return {
-        getActivePlayer, 
-        playRound, 
-        getBoard: board.getBoard,
-        getLastMessage,
-        startNewGame
-    }
-}
-
-
-function ScreenController () {
-    let game;
-    const playerTurnDiv = document.querySelector(".turn");
+const screenController = () => {
+    const game = gameController();
     const boardDiv = document.querySelector(".board");
-    const messageDiv = document.querySelector(".message");
-    const restartButtons = document.querySelectorAll(".restart");
-    const popupDiv = document.querySelector(".popup");
-
-    const showPopup = () => {
-        popupDiv.style.display = "flex";
-        messageDiv.textContent = game.getLastMessage();
-    }
-
-    const hidePopup = () => {
-        popupDiv.style.display = "none";
-        messageDiv.textContent = "";
-    }
-
-    const updateScreen = () => {
+    const statusDiv = document.querySelector(".status");
+    const restartButton = document.querySelector(".restart")
+    
+    const render = () => {
+        //Render game board
         boardDiv.textContent = "";
-
         const board = game.getBoard();
-        const activePlayer = game.getActivePlayer();
-
-        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
-        
-        board.forEach((row, rowIndex) => {
-            row.forEach((cell, columnIndex) => {
-                const cellButton = document.createElement("button");
-                cellButton.classList.add(`cell`);
-                cellButton.dataset.row = rowIndex;
-                cellButton.dataset.column = columnIndex;
-                cellButton.textContent = cell.getValue();
-                boardDiv.appendChild(cellButton);
-            })
+        board.forEach((cell, index) => {
+            const cellDiv = document.createElement("button");
+            cellDiv.classList.add("cell");
+            cellDiv.dataset.index = index;
+            cellDiv.textContent = cell.marker;
+            boardDiv.appendChild(cellDiv);
         })
 
-        if (game.getLastMessage().length) {
-            showPopup();
+        // Render status
+        const result = game.getResult();
+        const activePlayer = game.getActivePlayer().name;
+        if (result === true) {
+            statusDiv.textContent = `${activePlayer} wins!`;
+        } else if (result === "tie") {
+            statusDiv.textContent = "It's a tie!";
+        } else {
+            statusDiv.textContent = `${activePlayer}'s turn`;
         }
     }
-    
-    const resetScreen= () => {
-        let playerOneName;
-        let playerTwoName;
-        game = GameController(playerOneName, playerTwoName);
-        game.startNewGame();
-        hidePopup();
-        updateScreen();
-    }
-
-    const clickHandleBoard = (event) => {
-        const selectedColumn = parseInt(event.target.dataset.column);
-        const selectedRow = parseInt(event.target.dataset.row);
-        
-        if (isNaN(selectedColumn) || isNaN(selectedRow)) return;
-
-        game.playRound(selectedRow, selectedColumn);
-        updateScreen();
-    }
-
-    boardDiv.addEventListener('click', clickHandleBoard);
-    restartButtons.forEach(restartButton => {
-        restartButton.addEventListener("click", resetScreen);
+   
+    boardDiv.addEventListener("click", (event) => {
+        if (!event.target.classList.contains("cell")) return;
+        const index = event.target.dataset.index;
+        game.makeMove(index);
+        render();
     })
 
-    resetScreen();
+    restartButton.addEventListener("click", () => {
+        game.resetBoard();
+        render();
+    })
+    
+    render();
 }
 
-ScreenController();
+screenController();
